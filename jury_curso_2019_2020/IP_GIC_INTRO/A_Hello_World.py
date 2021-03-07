8d2fd97aedeb7c2cb7dbcca7527fcbb082dd1ef5
IvanMGM <ivanmgm88@gmail.com>
2021-03-07 01:50:26 +0100

    realizados algortimos de Ordenación Topológica y Grafo traspuesto en proceso Algortimo de Descomposición Componentes Fuertemente Conexas

--- jury_curso_2019_2020/IP_GIC_INTRO/A_Hello_World.py
+++
@@ -1,7 +0,0 @@
-############################################
-# TITLE: Hello World
-# AUTHOR: Iván Martín Gómez
-# DATE: Saturday 27th February
-# DESCRIPTION:
-############################################
-print ("Hello World!")
